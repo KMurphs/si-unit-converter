9e371fc5b4c94d997817a6e778823a3eb9fbe0ef
stephane kibonge <stephane.kibonge@etion.co.za>
2020-02-21 21:40:12 +0200

    Initial Commit: Flask is up and running

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # si-unit-converter
 A Tool to help students with SI unit conversions
+
+
+## Backend 
+The Backend is powered by python's Flask web framework
+
+### Getting Started
+https://flask.palletsprojects.com/en/1.1.x/installation/#installation
+https://flask.palletsprojects.com/en/1.1.x/quickstart/
---
+++ utils/setup.bat
@@ -0,0 +1,24 @@
+REM Initial Setup
+py -3 -m venv venv
+venv\Scripts\activate
+pip install Flask
+pip freeze > requirements.txt
+
+
+REM Prepare Repo for python coding
+cd .\venv\Scripts
+activate
+cd ..\..
+
+REM Create Minimal Application from 
+REM https://flask.palletsprojects.com/en/1.1.x/quickstart/
+REM into 'app.py'
+
+REM Prepare App For Running
+set FLASK_APP=app.py
+set MONGOPASS=Murphy321!
+set FLASK_DEBUG=1
+
+
+REM Run App
+flask run
